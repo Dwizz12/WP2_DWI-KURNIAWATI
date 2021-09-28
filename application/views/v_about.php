1d6df360ce5297d109aeaa9aa2aef2e2e002d0a8
Dwizz12 <dwikurniawati8602@gmial.com>
2021-09-28 17:24:02 +0700

    Membuat template sederhana yang dinamis

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Dwi</li>
+        <li>Nama Belakang : Kurniawati</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Desa Kuala Dua Komplek Alas Kusuma</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Kuala Dua</li>
+    </ul>
+    
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>Bulu Tangkis</li>
+    </ul>
+</section>
